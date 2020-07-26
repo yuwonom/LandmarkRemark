0c3e56e1e65a882b7243450fbd1197b32eff982c
Michael Yuwono <michaeloy1995@gmail.com>
2020-07-26 20:17:07 +1000

    Initial Commit

---
+++ LandmarkRemark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandmarkRemark
+//
+//  Created by Michael Yuwono on 26/7/20.
+//  Copyright © 2020 Michael Yuwono. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
